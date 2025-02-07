4f4498b1217679a3231d1f8e0b454b90f7df7510
kknyapple <72698829+kknyapple@users.noreply.github.com>
2025-02-07 10:56:47 +0900

    [Gold IV] Title: 거짓말, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.cc
+++ 백준/Gold/1043. 거짓말/거짓말.cc
@@ -1,80 +1,74 @@
 #include <iostream>
-#include <algorithm>
+#include <string>
 #include <vector>
+#include <algorithm>
 #include <queue>
 #include <tuple>
+#include <stack>
+#include <set>
 using namespace std;
-using ll = long long;
 
 int n, m;
-//int a[54][54];
-int party_people, real_people;
-int party[54];
-vector <int> real;
-vector <int> a[54];
-int parent[54];
-int res;
+vector <int> v[54];
+int p[54];
+int num, a;
+bool flag;
+int ans;
 
-int find_p(int x) {
-    if (x != parent[x]) return parent[x] = find_p(parent[x]);
-    else return parent[x];
+int find(int x) {
+    if (p[x] == x) return x;
+    return p[x] = find(p[x]);
 }
-// x와 y를 같은 그룹으로 만들어줌
-void mer(int x, int y) {
-    int px = find_p(parent[x]);
-    int py = find_p(parent[y]);
-    if (px != py) {
-        if (px < py)
-            parent[py] = parent[px];
-        else
-            parent[px] = parent[py];
-    }
+
+bool merge(int u, int v) {
+    u = find(u);
+    v = find(v);
+
+    if (u == v) return false;
+
+    if (p[v] < p[u]) swap(u, v);
+    else if (p[v] == p[u]) p[u]--;
+    p[v] = u;
+    return true;
 }
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
+    ios::sync_with_stdio(0); cin.tie(0);
+     
     cin >> n >> m;
-    for (int i = 1; i <= n; i++) {
-        parent[i] = i;
+    for (int i = 1;i <= n;i++) {
+        p[i] = i; // 부모 초기화
     }
-    cin >> real_people;
-    int t;
-    for (int i = 1; i <= real_people; i++) {
-        cin >> t;
-        parent[t] = 0;
+    cin >> num;
+    for (int i = 0;i < num;i++) {
+        cin >> a;
+        p[a] = 0; // 진실을 아는 사람을 0번과 연결
     }
-    int par;
-    for (int i = 1; i <= m; i++) {
-        cin >> party_people;
-        for (int j = 1; j <= party_people; j++) {
-            cin >> t;
-            a[i].push_back(t);
-            if (j == 1) par = t;
-            else {
-                mer(par, t);
-            }
-            
+    
+    // 파티 참여하는 사람들 merge
+    int tmp = 0;
+    for (int i = 0; i < m; i++) {
+        cin >> num;
+        for (int j = 0; j < num; j++) {
+            cin >> a;
+            v[i].push_back(a); // 파티 정보 저장
+            if (j == 0) tmp = a; // 처음 입력된 사람 번호 저장
+            else merge(tmp, a); // 파티 오는 사람들 merge
         }
     }
 
-    res = m;
-    if (real_people == 0) {
-        cout << res << "\n";
-        return 0;
-    }
-    
-    for (int i = 1; i <= m; i++) {
-        for (auto k : a[i]) {
-            if (find_p(parent[k])==0) {
-                res--;
+    // 파티 마다 진실을 알고 있는 지 판별
+    for (int i = 0;i < m;i++) {
+        flag = true;
+        for (int k : v[i]) {
+            if (find(k) == 0) { // 0번과 연결된 사람은 진실을 아는 사람
+                flag = false;
                 break;
             }
         }
+        if (flag) ans++;
     }
-
-    cout << res << "\n";
+    cout << ans << "\n";
 
     return 0;
 }
